6cf5c14913dc0c9a12b47fe9303d6fccc120e2ff
dannydelott <dannydelott@gmail.com>
2015-02-15 19:07:09 -0800

    Add shuffle fade in/out animation

--- insertionsort.js
+++ insertionsort.js
@@ -3,26 +3,26 @@ $(function(){
   /* ******************
    * GLOBAL VARIABLES *
    * ******************/
-
-  var $ace = $('#ace');
-  var $two = $('#two');
-  var $three = $('#three');
-  var $four = $('#four');
-  var $five = $('#five');
-  var deck = [$ace, $two, $three, $four, $five];
+  var $playingCards = $('.playingCards');
+  var $table = $('.table');
 
   /* ******************
    * HELPER FUNCTIONS *
    * ******************/
-
+  // Generate a random number
   var rand = function(max){
     return Math.floor(Math.random() * max);
   }; 
 
+  // Fade out, shuffle cards, fade in
   var shuffleCards = function(){
-    _.each(deck, function(card, key, collection){
-      card.insertAfter(collection[rand(4)]);
+    $playingCards.fadeOut('slow', function(){
+      _.each($table.children(), function(card){
+        $(card).insertBefore($table.children()[rand(4)]);
+      });
+      $playingCards.fadeIn('slow');
     });
+
   };
 
   /* **************
